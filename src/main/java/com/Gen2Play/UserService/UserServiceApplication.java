a4d51cda32538b580ea215e5f42cb0b95ab13ac9
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-02-24 16:47:07 +0700

    add Dockerfile and fix eureka

--- src/main/java/com/Gen2Play/UserService/UserServiceApplication.java
+++ src/main/java/com/Gen2Play/UserService/UserServiceApplication.java
@@ -3,14 +3,12 @@ package com.Gen2Play.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		// Dotenv dotenv = Dotenv.load();
+		// dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
